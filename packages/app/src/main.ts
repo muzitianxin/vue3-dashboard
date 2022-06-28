aa42504eb44dbf1316d0a52f7f801a919a242c5f
muzitianxin <1078937703@qq.com>
2022-06-28 08:56:44 +0800

    feat(v-count-up): 将CountUp.js封装为自定义指令

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,21 +1,23 @@
 import "animate.css";
 import { createApp } from "vue";
 import { Quasar } from "quasar";
-import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
 
 import App from "./App.vue";
 import router from "./router/index";
 import quasarOptions from "./quasar";
 
+import { vCountUp } from "./directives/count-up.directive";
 import { registerGlobalComponents } from "./global-components";
+import { vClickOutSide } from "./directives/click-outside.directive";
 
 import "./styles/common.scss";
 import "./styles/variables.scss";
-import { vClickOutSide } from "./directives/click-outside.directive";
 
 export const vm = registerGlobalComponents(createApp(App))
   .use(Quasar, quasarOptions)
   .use(router)
   .use(autoAnimatePlugin)
   .directive("click-outside", vClickOutSide)
+  .directive("count-up", vCountUp)
   .mount("#app");
