5e58b34435c96122dd9922209a89ed3170492427
muzitianxin <1078937703@qq.com>
2022-09-01 22:03:33 +0800

    chore(app/packages.json): set base path of building github-pages

---
+++ docs/assets/index.27e760d8.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as r}from"./index.fa1d954e.js";const n={};function t(a,o){return r(),c("div",null,"Chat")}var s=e(n,[["render",t]]);export{s as default};
---
+++ docs/assets/index.29f0b14e.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./index.fa1d954e.js";const r={};function t(o,a){return n(),c("div",null,"Setting")}var s=e(r,[["render",t]]);export{s as default};
