7ba711d6fcbd7da5df73482f2da928ebdb6825d6
muzitianxin <1078937703@qq.com>
2022-09-01 21:54:15 +0800

    Revert "chore: change outDir to "/docs" for github pages"
    This reverts commit 06efe9f0526c46813866cfd68f4b5c002010d0c7.

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -6,9 +6,6 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: path.join(__dirname, "../../", "docs"),
-  },
   plugins: [
     vue({
       template: { transformAssetUrls },
